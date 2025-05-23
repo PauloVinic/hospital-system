285ba395f6cfb3fe811823399df16e627e436b06
PauloVinic <paulo_vinicius_21@hotmail.com>
2025-05-22 23:06:34 -0300

    fix: atualiza Lombok para v1.18.38 e configura VS Code

--- agendamento-service/src/main/java/com/techchallenge/agendamentoservice/controller/ConsultaController.java
+++ agendamento-service/src/main/java/com/techchallenge/agendamentoservice/controller/ConsultaController.java
@@ -1,5 +1,17 @@
 package com.techchallenge.agendamentoservice.controller;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.techchallenge.agendamentoservice.domain.Consulta;
 import com.techchallenge.agendamentoservice.dto.ConsultaPageResponseDTO;
 import com.techchallenge.agendamentoservice.dto.ConsultaRequestDTO;
@@ -9,13 +21,6 @@ import com.techchallenge.agendamentoservice.service.ConsultaService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.data.domain.Sort;
 
 @RestController
 @RequestMapping("/consultas")
